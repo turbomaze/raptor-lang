99ca0c69c5e008cbe0fab012456e8bcbbfb66415
Anthony Liu <anthonyscottliu@gmail.com>
2016-07-10 23:14:46 -0400

    Adds support for interpreting variable assignments and expressions.

--- index.js
+++ index.js
@@ -11,24 +11,24 @@
 var util = require('util');
 var Parser = require('./lib/parser.js').Parser;
 var Interpreter = require('./lib/interpreter.js').Interpreter;
-var langGrammar = require('./grammar/language-grammar.js');
-var langStructure = require('./grammar/language-structure.js');
+var langGrammar = require('./grammar/language-grammar.js').grammar;
+var langStructure = require('./grammar/language-structure.js').structure;
 
 // working variables
 var parser = new Parser(langGrammar, langStructure);
-var interpreter = new Interpreter(parser);
+var interpreter = new Interpreter(langGrammar, langStructure);
 
 var goal = 'program';
 var input = `
 triple => x {
-  31 > x + 3 {
-    return 2 * x
-  }
-  return 0
+  return 3*x
 }
 
-let a = 10
-log -> triple -> triple -> a
+a = 10
+b = a * 16 + 4
+a = 6 * b * 2 + 5
+b = 1974
+c = a < b
 `;
 
 
@@ -37,8 +37,8 @@ var ast = parser.parse(goal, input.split(''));
 console.log("Source:");
 console.log(input.trim());
 
-console.log("\nAST:");
-console.log(util.inspect(ast, false, null));
+// console.log("\nAST:");
+// console.log(util.inspect(ast, false, null));
 
 console.log("\nInterpreting...");
 console.log(interpreter.interpret(input));
